36d01875b5ab6ffc267675dc10e14e857bf5c490
mac <mac@macui-MacBook-Pro.local>
2017-01-24 02:12:50 +0900

    to the end of login and signup ui

--- practice/my_test/main.py
+++ practice/my_test/main.py
@@ -1,22 +1,52 @@
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, redirect, request, render_template, url_for, session, jsonify
+from flask_oauthlib.client import OAuth
+
+
 from user import User
 from registration import RegistrationForm
 
+CLIENT_ID = 'Xi6k7bRQZf1XrQIqoYRL687QgByYht7O5K8Mijmz'
+CLIENT_SECRET = 'HSdWIxtLtNmt6ET6hJn6ZluFu6yGTH13h3zEBedrK4dtXhOVLm'
+
+
 # configuration
 DEBUG = True
+SECRET_KEY = 'my hi secret'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+oauth = OAuth(app)
+
+myssu = oauth.remote_app(
+    'myssu',
+    consumer_key=CLIENT_ID,
+    consumer_secret=CLIENT_SECRET,
+    request_token_params={'scope': 'email'},
+    request_token_url=None,
+    base_url='http://127.0.0.1:5000/api/',
+    access_token_url='http://127.0.0.1:5000/oauth/token',
+    authorize_url='http://127.0.0.1:5000/oauth/authorize',
+    # base_url='https://api.yourssu.com/api/',
+    # access_token_url='https://api.yourssu.com/oauth/token',
+    # authorize_url='https://api.yourssu.com/oauth/authorize'
+)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET':
-        print(request.args.get("a"))
-    else:
-        print(request.form.get('email'))
-        # print(request.form.email.data) # can't
-    return render_template('login.html')
+    # next_url = request.args.get('next') or request.referrer or None
+    next_url = None # 왜 None일때만 되는가?
+    print('next_url', next_url)
+    return myssu.authorize(
+        callback=url_for('authorized', next=next_url, _external=True)
+    )
 
+@app.route('/account/signup', methods=['GET', 'POST'])
+def account_signup():
+    next_url = None # 왜 None일때만 되는가?
+    print('next_url', next_url)
+    return myssu.authorize(
+        callback=url_for('authorized', next=next_url, _external=True)
+    )
 
 @app.route('/register_wtforms', methods=['GET', 'POST'])
 def register_wtform():
@@ -33,10 +63,26 @@ def register_wtform():
         return render_template('register_wtforms.html', form=form)
 
 
+@app.route('/authorized')
+def authorized():
+    print('authorized')
+    resp = myssu.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    print(resp)
+    session['remote_oauth'] = (resp['access_token'], '')
+    return jsonify(oauth_token=resp['access_token'])
+
 @app.route('/')
 def index():
-    return 'hello world!!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(port=5112)
+    import os
+    os.environ['DEBUG'] = 'true'
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+    app.run(host='localhost', port=8000)
