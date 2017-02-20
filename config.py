a3f4c6f9c9e9866985c1aeb72b292e19fbf5d4d0
Ubuntu <ubuntu@ip-172-31-6-124.ap-northeast-2.compute.internal>
2017-02-20 14:39:59 +0000

    fix config

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 MYSQL_DB_NAME = os.environ['MYSQL_DB_NAME']
 MYSQL_DB_ROOT = os.environ['MYSQL_DB_ROOT']
 MYSQL_DB_PS = os.environ['MYSQL_DB_PS']
+
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECERT = os.environ['CLIENT_SECRET']
+
