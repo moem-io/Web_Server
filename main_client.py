3e2d2c221d50c284b0080738a033eb050ca13437
lawiparadise <lawiparadise@gmail.com>
2017-02-27 21:50:40 +0000

    fix

--- main_client.py
+++ main_client.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     import os
 
     os.environ['DEBUG'] = 'true'
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     app.run(host='0.0.0.0', port=8000)
