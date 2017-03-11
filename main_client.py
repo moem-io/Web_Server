1874d7ed37fa90c9ac8705ccef092d9ad6fda7cd
lawiparadise <lawiparadise@gmail.com>
2017-03-11 18:08:25 +0900

    fix much

--- main_client.py
+++ main_client.py
@@ -7,3 +7,5 @@ if __name__ == '__main__':
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     app.run(host='0.0.0.0', port=8000, debug=True)
     # app.run(host='0.0.0.0', port=8000)
+    # app.run(port=8000, debug=True)
+
--- my_client/routes/index.py
+++ my_client/routes/index.py
@@ -5,16 +5,36 @@ from requests import get, post, put, delete, Request
 from my_client.forms.board import writingForm
 from my_client.routes.oauth import remote
 
-url = 'http://localhost:5000/board'
-
+# url = 'http://localhost:5000/board'
+# url = 'http://13.124.19.161:5000/board'
+url_hub = 'http://localhost:5000/hub_status'
+url_board = 'http://localhost:5000/board'
 
 @app.route('/')
 def index():
     resp = None
     if 'remote_oauth' in session:
         resp = remote.get('me')
-    return render_template('index_light.html', resp=resp)
+    username = None
+    if resp:
+        username = resp.data.get('username')
+    print(username)
 
+    res = None
+    if username:
+        payload = {'username': username}
+        response = post(url_hub, json=payload)
+        print(response.text)
+        res = response.text
+        # print(str(res.text))
+
+        # return render_template('index2.html', res=res.text)
+    # return render_template('index2.html', res=res)
+    data = {
+        'username':username,
+        'res':res,
+    }
+    return render_template('index2.html', data=data)
 
 @app.route('/board', methods=['GET', 'POST'])
 def board():
@@ -24,7 +44,7 @@ def board():
         payload = {'title': title}
         # res = post(url, data=json.dumps(payload)) # 안 됨
         # res = post(url, data=payload) # 되긴 하는데 json아님
-        res = post(url, json=payload)
+        res = post(url_board, json=payload)
 
         #Todo 여기 성공 실패 분기 해야 함
         print(res)
@@ -33,5 +53,3 @@ def board():
         return str(res.text)
 
     return render_template('board.html', form=form)
-
-
