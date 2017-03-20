4f4058190b7b0ecd5128050b19c6ce5dd581a4b5
lawiparadise <lawiparadise@gmail.com>
2017-03-20 21:36:53 +0900

    fix signin

--- main_client.py
+++ main_client.py
@@ -1,10 +1,10 @@
 from my_client.app import app
 
 if __name__ == '__main__':
-    # import os
-    # os.environ['DEBUG'] = 'true'
-    # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+    import os
+    os.environ['DEBUG'] = 'true'
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
 
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    # app.run(host='0.0.0.0', port=8000, debug=True)
     # app.run(host='0.0.0.0', port=8000)
-    # app.run(port=8000, debug=True)
+    app.run(port=8000, debug=True)
